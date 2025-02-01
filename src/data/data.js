b2be78a926b6824b71130596d6f99002322b280d
Adaobilynda1234 <adaobilynda1234@gmail.com>
2025-02-01 20:12:49 +0100

    first commit

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 import Trakmama from "../assets/projects/Trakmamawebsite.jpg";
 import Dictionary from "../assets/projects/Dictionary.jpg";
+import Quiz from "../assets/projects/Quizapp.jpg";
 
 export const data = [
   {
@@ -11,7 +12,7 @@ export const data = [
   },
   {
     id: 2,
-    name: "React JS Application",
+    name: "JS Application",
     image: Dictionary,
     github: "https://github.com/Adaobilynda1234/Dictionary-app",
     live: "https://effortless-blini-885ee1.netlify.app/",
@@ -19,9 +20,9 @@ export const data = [
   {
     id: 3,
     name: "React JS Application",
-    image: Trakmama,
-    github: "https://github.com/Adaobilynda1234/Trakmama-project",
-    live: "https://trakmama-project.vercel.app/",
+    image: Quiz,
+    github: "https://github.com/Adaobilynda1234/React-Quiz-App",
+    live: "https://react-quiz-app-sigma-inky.vercel.app/",
   },
   {
     id: 4,
